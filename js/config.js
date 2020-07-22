c3d97fff56b89947d5a491f5e374d024059cacc5
traaittPlatform <traaittPlatform@github.com>
2020-07-22 03:03:47 +0200

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const ExplorerConfig = {
+  apiBaseUrl: 'http://explorer.chimeraproject.io/',
+  ticker: 'CMRA',
+  maxSupply: 100000000000,
+  emissionSpeed: 21,
+  decimalPoints: 2,
+  blockTargetTime: 60,
+  startYear: 2019,
+  genesisTimestamp: 1512800692,
+  emissionCurveInterval: 20000,
+  emissionCurveDataPoints: 15000,
+  addressPrefix: 4141013,
+  forkHeights: [
+    1200000
+  ]
+}
